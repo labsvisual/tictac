c0f8127588a2e6d309dec651a6d37cd761201c3e
Shreyansh Pandey <shreyansh.pandey@recro.io>
2019-03-15 14:35:34 +0530

    refractor(core): refractor the code to remove redundancy and some bugs

--- index.js
+++ index.js
@@ -14,78 +14,101 @@
 * Winner needs to be decided and has to be flashed
 *
 * Extra points will be given for approaching the problem more creatively
-* 
+*
 */
 
-const grid = [];
 const GRID_LENGTH = 3;
-let turn = 'X';
+const grid = new Array( GRID_LENGTH ).fill( -1 ).map( el => new Array( GRID_LENGTH ).fill( -1 ) );
 
-function initializeGrid() {
-    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
-        const tempArray = [];
-        for (let rowidx = 0; rowidx < GRID_LENGTH;rowidx++) {
-            tempArray.push(0);
-        }
-        grid.push(tempArray);
-    }
-}
+function getRowBoxes( colIndex ) {
 
-function getRowBoxes(colIdx) {
-    let rowDivs = '';
-    
-    for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
-        let additionalClass = 'darkBackground';
-        let content = '';
-        const sum = colIdx + rowIdx;
-        if (sum%2 === 0) {
-            additionalClass = 'lightBackground'
-        }
-        const gridValue = grid[colIdx][rowIdx];
-        if(gridValue === 1) {
-            content = '<span class="cross">X</span>';
-        }
-        else if (gridValue === 2) {
-            content = '<span class="cross">O</span>';
-        }
-        rowDivs = rowDivs + '<div colIdx="'+ colIdx +'" rowIdx="' + rowIdx + '" class="box ' +
-            additionalClass + '">' + content + '</div>';
+    const rowDivs = [];
+
+    const renderRow = ( { colIndex, rowIndex, additionalClass, content } ) => (
+        `<div data-col-index="${ colIndex }" data-row-index="${ rowIndex }" ` +
+        `class="${ [ 'box', additionalClass ].join( ' ' ) }">` +
+        `${ content }</div>`
+    );
+
+    for ( let rowIndex = 0; rowIndex < GRID_LENGTH; rowIndex++ ) {
+
+        const additionalClass = ( colIndex + rowIndex ) % 2 === 0 ? 'light-background' : 'dark-background';
+        const boxValue = grid[ colIndex ][ rowIndex ];
+
+        const content = `<span class="cross">${
+            boxValue === 0 ? 'X' :
+                boxValue === 1 ? 'O' : ''
+        }</span>`;
+
+        rowDivs.push( renderRow( { colIndex, rowIndex, additionalClass, content } ) );
     }
-    return rowDivs;
+
+    return rowDivs.join( '' );
+
 }
 
 function getColumns() {
-    let columnDivs = '';
-    for(let colIdx=0; colIdx < GRID_LENGTH; colIdx++) {
-        let coldiv = getRowBoxes(colIdx);
-        coldiv = '<div class="rowStyle">' + coldiv + '</div>';
-        columnDivs = columnDivs + coldiv;
+
+    const columnDivs = [];
+
+    for ( let colIndex = 0; colIndex < GRID_LENGTH; colIndex++ ) {
+
+        columnDivs.push( `<div class="row-style">${ getRowBoxes( colIndex ) }</div>` );
+
     }
-    return columnDivs;
+
+    return columnDivs.join( '' );
 }
 
 function renderMainGrid() {
-    const parent = document.getElementById("grid");
-    const columnDivs = getColumns();
-    parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+
+    const parent = document.getElementById( 'grid' );
+    const columnsNode = document.createElement( 'div' );
+
+    columnsNode.className = 'columns-style';
+    columnsNode.innerHTML = getColumns();
+
+    // If you are recursively adding event listeners, it's better
+    // to remove either the root object (child nodes) and/or the
+    // the child node.
+
+    const innerGrid = parent.childNodes[ 0 ];
+
+    if ( innerGrid ) {
+
+        innerGrid.removeEventListener( 'click', onBoxClick );
+
+    }
+
+    parent.removeChild( innerGrid || null );
+    parent.appendChild( columnsNode );
+
 }
 
 function onBoxClick() {
-    var rowIdx = this.getAttribute("rowIdx");
-    var colIdx = this.getAttribute("colIdx");
-    let newValue = 1;
-    grid[colIdx][rowIdx] = newValue;
+
+    const rowIndex = this.getAttribute( 'data-row-index' );
+    const colIndex = this.getAttribute( 'data-col-index' );
+
+    let newValue = 0;
+
+    grid[colIndex][rowIndex] = newValue;
     renderMainGrid();
+
+    // Possible memory leak if you don't remove
+    // the reference to the child node.
     addClickHandlers();
 }
 
 function addClickHandlers() {
-    var boxes = document.getElementsByClassName("box");
-    for (var idx = 0; idx < boxes.length; idx++) {
-        boxes[idx].addEventListener('click', onBoxClick, false);
-    }
+
+    Array.from( document.getElementsByClassName( 'box' ) ).forEach( el => {
+
+        el.addEventListener( 'click', onBoxClick );
+
+    } );
+
 }
 
-initializeGrid();
 renderMainGrid();
 addClickHandlers();
